d62216fcb64b1bc899f276a51c0049da38baff8e
Cristian F. Tovar <ctovar@abexuspr.com>
2020-08-08 15:11:01 -0500

    Setting up our project
    App with bootstrap because the teacher is a lazy-ass :(

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import ChartWrapper from './ChartWrapper';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 class App extends Component {
 
   render(){
     return (
-      <ChartWrapper />
+      <>
+        <Navbar bg="light">
+            <Navbar.Brand>Barchartly</Navbar.Brand>
+        </Navbar>
+        <Container>
+
+        </Container>
+        <ChartWrapper />
+      </>
     );
   }
 }
