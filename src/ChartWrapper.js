c6908d76ff7aa9b71c18160988f63701f8ab0ea8
Cristian F. Tovar <ctovar@abexuspr.com>
2020-08-08 11:27:16 -0500

    Adding SVGs with D3
    This is an explanation of the D3 tree's structure when we use its methods

--- src/ChartWrapper.js
+++ src/ChartWrapper.js
@@ -8,10 +8,7 @@ export default class ChartWrapper extends Component {
 
 	render() {
 		return <div ref="chart">
-			<svg width='150' height='100'>
-				<circle cx='50' cy='50' r='20' fill='blue'/>
-				<rect x='70' y='50' width='60' height='10' fill='purple'/>
-			</svg>
+			
 		</div>
 	}
 }
--- src/D3Chart.js
+++ src/D3Chart.js
@@ -1,5 +1,7 @@
+import * as d3 from 'd3';
+
 export default class D3chart {
   constructor(element){
-    console.log("Hello World");
+    console.log(d3.select(element));
   }
 }
