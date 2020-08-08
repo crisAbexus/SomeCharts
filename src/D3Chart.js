dd4b89fe3ca1a25705b8caad6a07113f3ae600e7
Cristian F. Tovar <ctovar@abexuspr.com>
2020-08-08 13:42:46 -0500

    Working with external data
    The elements changes their color by the name attribute

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -1,5 +1,4 @@
 import * as d3 from 'd3';
-const data = [20, 12, 16, 25, 20];
 const url = 'https://udemy-react-d3.firebaseio.com/ages.json'
 
 export default class D3chart {
@@ -10,16 +9,21 @@ export default class D3chart {
       .attr('height', 500);
 
     d3.json(url).then(agesData => {
-      console.log(agesData);
+      const rects = svg.selectAll('rect')
+        .data(agesData)
+  
+      rects.enter().append('rect')
+        .attr('x', (d, i) => i * 100)
+        .attr('y', 50)
+        .attr('width', 50)
+        .attr('height', d => d.age *10)
+        .attr('fill', d => {
+          if(d.name == 'Tony'){
+            return 'red'
+          }else {
+            return 'green'
+          }
+        })
     })
-    const rects = svg.selectAll('rect')
-      .data(data)
-
-    rects.enter().append('rect')
-      .attr('x', (d, i) => i * 100)
-      .attr('y', 50)
-      .attr('width', 50)
-      .attr('height', d => d)
-      .attr('fill', 'grey')
   }
 }
