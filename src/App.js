c620d91b95b5b3f199cdd2cd742a86e971a9fe13
Cristian F. Tovar <ctovar@abexuspr.com>
2020-08-08 07:21:05 -0500

    First commit
    This first commit has some code according to the first class coding example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Test from './Test';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    personClicks: 0
+  }
+
+  personClicked() {
+    this.setState({ personClicks: this.state.personClicks +1 });
+    // DO NOT DO THIS
+    this.state.personClicks = this.state.PersonClicks + 1 
+  }
+  render(){
+    return (
+      <div className="App">
+        <Test person="Tony" />
+        <Test person="Mary" />
+        <Test person="Susan" />
+        Person Clicks: {this.state.personClicks}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Test.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Test = ({ person }) => {
+  return <div>Hello {person}</div>
+}
+
+export default Test;
