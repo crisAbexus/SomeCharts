4440db9b3eb69631983ac0f0821fa75d01f0f7fd
Cristian F. Tovar <ctovar@abexuspr.com>
2020-08-08 20:32:08 -0500

    Writing our bars with scales
    padding of the bars setted

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -1,4 +1,5 @@
 import * as d3 from 'd3';
+
 const url = 'https://udemy-react-d3.firebaseio.com/tallest_men.json'
 
 export default class D3chart {
@@ -7,21 +8,25 @@ export default class D3chart {
       .append('svg')
         .attr('width', 800)
         .attr('height', 500)
+
     d3.json(url).then(data => {
       const y = d3.scaleLinear()
         .domain([0, 272])
         .range([0, 500])
 
-      console.log(`y(100)=${y(100)}\ny(0)=${y(0)}\ny(272)=${y(272)} `)
+      const x = d3.scaleBand()
+        .domain(data.map(d => d.name))
+        .range([0, 800])
+        .padding(0.4)
 
       const rects = svg.selectAll('rect')
         .data(data)
 
       rects.enter().append('rect')
-        .attr('x', (d,i)=> i*100)
+        .attr('x', d => x(d.name))
         .attr('y', 0)
-        .attr('width', 50)
-        .attr('height', d => d.height)
+        .attr('width', x.bandwidth)
+        .attr('height', d => y(d.height))
         .attr('fill','grey')
     })
   }
