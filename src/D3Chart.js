fc49214ba5e306d351b1d3b5360fe39e545c5a45
Cristian F. Tovar <ctovar@abexuspr.com>
2020-08-09 13:22:12 -0500

    Adding updates to our visualization
    It changed a lot

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -7,62 +7,71 @@ const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTOOM;
 
 export default class D3chart {
   constructor(element) {
-    const svg = d3.select(element)
+    const vis = this
+
+    vis.svg = d3.select(element)
       .append('svg')
         .attr('width', WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
         .attr('height', HEIGHT + MARGIN.TOP + MARGIN.BOTOOM)
       .append("g")
         .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
-        
-    d3.interval(() => {
-      console.log('updating');
-      
-    },1000)
 
-    d3.json(url).then(data => {
-      
-      const y = d3.scaleLinear()
-        .domain([
-          d3.min(data,d => d.height) * 0.95,
-          d3.max(data, d => d.height)
-        ])
-        .range([HEIGHT, 0])
-
-      const x = d3.scaleBand()
-        .domain(data.map(d => d.name))
-        .range([0, WIDTH])
-        .padding(0.4)
-
-      const xAxisCall = d3.axisBottom(x)
-      svg.append('g')
-        .attr('transform', `translate(0, ${HEIGHT})`)
-        .call(xAxisCall)
-
-      const yAxisCall = d3.axisLeft(y)
-      svg.append('g').call(yAxisCall)
-
-      svg.append('text')
-        .attr('x',WIDTH/2)
+    vis.svg.append('text')
+        .attr('x', WIDTH / 2)
         .attr('y', HEIGHT + 50)
         .attr('text-anchor', 'middle')
-        .text('The world\'s tallest men')
-      
-      svg.append('text')
+      .text('The world\'s tallest men')
+
+    vis.svg.append('text')
         .attr('x', -(HEIGHT / 2))
         .attr('y', -50)
         .attr('text-anchor', 'middle')
-        .text('Height in cm')
+      .text('Height in cm')
         .attr('transform', 'rotate(-90)')
 
-      const rects = svg.selectAll('rect')
-        .data(data)
+    vis.xAxisGroup = vis.svg.append('g')
+        .attr('transform', `translate(0, ${HEIGHT})`)
 
-      rects.enter().append('rect')
-        .attr('x', d => x(d.name))
-        .attr('y', d => y(d.height))
-        .attr('width', x.bandwidth)
-        .attr('height', d => HEIGHT - y(d.height))
-        .attr('fill','grey')
+
+    vis.yAxisGroup = vis.svg.append('g')
+
+    d3.json(url).then(data => {
+      vis.data = data
+      d3.interval(() => {
+        vis.update()
+      }, 1000)
     })
+  }
+
+  update() {
+    const vis = this;
+
+    const y = d3.scaleLinear()
+      .domain([
+        d3.min(vis.data, d => d.height) * 0.95,
+        d3.max(vis.data, d => d.height)
+      ])
+      .range([HEIGHT, 0])
+
+    const x = d3.scaleBand()
+      .domain(vis.data.map(d => d.name))
+      .range([0, WIDTH])
+      .padding(0.4)
+
+    const xAxisCall = d3.axisBottom(x)
+    vis.xAxisGroup.call(xAxisCall)
+
+    const yAxisCall = d3.axisLeft(y)
+    vis.yAxisGroup.call(yAxisCall)
+
+    // const rects = vis.svg.selectAll('rect')
+    // .data(data)
+
+    // rects.enter().append('rect')
+    //  .attr('x', d => x(d.name))
+    //  .attr('y', d => y(d.height))
+    //  .attr('width', x.bandwidth)
+    //  .attr('height', d => HEIGHT - y(d.height))
+    //  .attr('fill', 'grey')
   }
 }
