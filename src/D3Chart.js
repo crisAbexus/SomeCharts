236ba0c0c28794dbbb487ee44dfc1d60d902a351
Cristian F. Tovar <ctovar@abexuspr.com>
2020-08-08 21:20:57 -0500

    Fixing our bar heights
    Using the d3-array properties to find the max value and use it as parameter

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -2,21 +2,25 @@ import * as d3 from 'd3';
 
 const url = 'https://udemy-react-d3.firebaseio.com/tallest_men.json'
 
+const WIDTH = 800;
+const HEIGHT = 500;
+
 export default class D3chart {
   constructor(element) {
     const svg = d3.select(element)
       .append('svg')
-        .attr('width', 800)
-        .attr('height', 500)
+        .attr('width', WIDTH)
+        .attr('height', HEIGHT)
 
     d3.json(url).then(data => {
+      
       const y = d3.scaleLinear()
-        .domain([0, 272])
-        .range([0, 500])
+        .domain([0, d3.max(data, d => d.height)])
+        .range([0, HEIGHT])
 
       const x = d3.scaleBand()
         .domain(data.map(d => d.name))
-        .range([0, 800])
+        .range([0, WIDTH])
         .padding(0.4)
 
       const rects = svg.selectAll('rect')
@@ -24,7 +28,7 @@ export default class D3chart {
 
       rects.enter().append('rect')
         .attr('x', d => x(d.name))
-        .attr('y', 0)
+        .attr('y', d => HEIGHT -y(d.height))
         .attr('width', x.bandwidth)
         .attr('height', d => y(d.height))
         .attr('fill','grey')
