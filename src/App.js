9de8366fd081a9faf233b473f8731b0f39ddfd4a
Cristian F. Tovar <ctovar@abexuspr.com>
2020-08-10 10:32:46 -0500

    Loading in our data
    During the loading stage, a 'No data yet' sign is poping up

--- src/App.js
+++ src/App.js
@@ -3,10 +3,29 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
+import { json } from 'd3'
 import ChartWrapper from './ChartWrapper';
 
+const URL = 'https://udemy-react-d3.firebaseio.com/children.json'
 class App extends Component {
+  state = {
+    data: []
+  }
+
+  componentWillMount() {
+    json(URL)
+      .then(data => {
+        this.setState({ data })        
+      })
+      .catch(error => console.log(error))
+  }
+
+  renderChart() {
+    if (this.state.data.length === 0) {
+      return "No data yet"
+    }
+    return <ChartWrapper data={this.state.data} />
+  }
   render() {
     return (
       <div>
@@ -15,7 +34,7 @@ class App extends Component {
         </Navbar>
         <Container>
           <Row>
-            <Col md={6} xs={12}><ChartWrapper /></Col>
+            <Col md={6} xs={12}>{this.renderChart()}</Col>
             <Col md={6} xs={12}></Col>
           </Row>
         </Container>
