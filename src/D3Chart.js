84d503861b6e3f66c229c4f3bef90ae2d22e2fb0
Cristian F. Tovar <ctovar@abexuspr.com>
2020-08-10 11:28:36 -0500

    Starting out scatterplot: adding scales
    watching vis.x(6) and d3.max(vis.data,d => d.age) behavor in the console.log()

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -7,15 +7,25 @@ const HEIGHT = 300 - MARGIN.TOP - MARGIN.BOTTOM;
 class D3Chart {
 	constructor(element, data) {
 		let vis = this
+		vis.data = data
 
-		console.log(data);
-		
 		vis.g = d3.select(element)
 			.append("svg")
 				.attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
 				.attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
 			.append("g")
 				.attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
+		
+		vis.x = d3.scaleLinear()
+			.domain([0, d3.max(vis.data, d => d.age)])
+			.range([0, WIDTH])
+		
+		vis.y = d3.scaleLinear()
+			.domain([0,d3.max(vis.data, d => d.height)])
+			.range([HEIGHT, 0])
+
+		console.log(`vis.x(6)=${vis.x(6)}`);
+		console.log(`d3.max(vis.data,d => d.age)=${d3.max(vis.data,d => d.age)}`);		
 
 		vis.update()		
 	}
