e09318150ae3885d31c8b14b8f12a48d03d3d37c
Cristian F. Tovar <ctovar@abexuspr.com>
2020-08-10 14:33:55 -0500

    Back to React - adding an input row
    Two fields one buttom

--- src/Table.js
+++ src/Table.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export default class Table extends Component {
 
 	render() {
-		return "Hello World"
+		return (
+			<Row>
+
+				<Col xs={3}>
+					<Form.Control 
+						placeholder={'Height'}
+						name={'height'}
+					/>
+				</Col>
+
+				<Col xs={3}>
+					<Form.Control 
+						placeholder={'Age'}
+						name={'age'}
+					/>
+				</Col>
+				
+				<Col xs={3}>
+					<Button
+						variant={'primary'}
+						type={'button'}
+						style={{ width:'100%' }}
+					>
+						Add
+					</Button>
+				</Col>
+
+			</Row>
+		)
 	}
 }
