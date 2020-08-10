b8b473cffabc9277c2d58d4b3ea691c8a0623d88
Cristian F. Tovar <ctovar@abexuspr.com>
2020-08-09 21:20:15 -0500

    Adding our dropdown with React
    React-Bootstrap col & row components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import ChartWrapper from './ChartWrapper';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ChartWrapper from './ChartWrapper';
+import GenderDropdown from './GenderDropdown';
 class App extends Component {
 
   render(){
@@ -11,7 +14,13 @@ class App extends Component {
             <Navbar.Brand>Barchartly</Navbar.Brand>
         </Navbar>
         <Container>
-          <ChartWrapper />
+          <Row>
+            <Col xs={12}><GenderDropdown /></Col>
+          </Row>
+          <Row>
+            <Col xs={12}><ChartWrapper /></Col>
+          </Row>
+          
         </Container>
       </>
     );
