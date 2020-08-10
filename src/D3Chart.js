08ff895aa7d6677b16583084153e646caa3b662b
Cristian F. Tovar <ctovar@abexuspr.com>
2020-08-10 13:52:32 -0500

    Adding axes and labels
    code done

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -22,6 +22,27 @@ class D3Chart {
 		vis.y = d3.scaleLinear()
 			.range([HEIGHT, 0])
 
+		vis.xAxisGroup = vis.g.append('g')
+			.attr('transform', `translate(0, ${HEIGHT})`);
+
+		vis.yAxisGroup = vis.g.append('g');
+
+		vis.g.append('text')
+			.attr('x', WIDTH / 2)
+			.attr('y', HEIGHT + 40)
+			.attr('font-size', 20)
+			.attr('text-anchor', 'middle')
+			.text('Age')
+
+		vis.g.append('text')
+			.attr('x', -(HEIGHT / 2))
+			.attr('y', -50)
+			.attr('transform', 'rotate(-90)')
+			.attr('font-size', 20)
+			.attr('text-anchor', 'middle')
+			.text('Height in cm')
+
+
 		vis.update()		
 	}
 
@@ -30,6 +51,12 @@ class D3Chart {
 		
 		vis.x.domain([0, d3.max(vis.data, d => Number(d.age) )])
 		vis.y.domain([0,d3.max(vis.data, d => Number(d.height) )])
+
+		const xAxisCall = d3.axisBottom(vis.x)
+		const yAxisCall = d3.axisLeft(vis.y)
+
+		vis.xAxisGroup.call(xAxisCall)
+		vis.yAxisGroup.call(yAxisCall)
 
 		// JOIN
 		const circles = vis.g.selectAll('circle')
